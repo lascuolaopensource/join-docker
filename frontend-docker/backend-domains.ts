da02d47f202b88edc937713e3cdf230474b51a87
U-ALESSANDRO-CLEV\Alessandro <alessandro.nge@gmail.com>
2019-11-20 21:39:16 +0100

    add files

---
+++ frontend-docker/backend-domains.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    backendUrl: 'https://pandev.lascuolaopensource.xyz/api',
+    rulesUrl: 'https://pandev.lascuolaopensource.xyz/rules',
+    auth: {
+      clientId: 'user',
+      redirectUri: 'auth.html',
+      authUrl: 'https:/pandev.lascuolaopensource.xyz/oauth2/access_token',
+      logoutUrl: 'https:/pandev.lascuolaopensource.xyz/signout'
+    }
+  };
+  
